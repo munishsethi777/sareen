b2d4330a1a6fd393d386b62693ef9f91168f01c9
munishsethi777 <munishsethi777@gmail.com>
2018-03-19 16:25:20 +0530

    create inventory

--- createInventory.php
+++ createInventory.php
@@ -6,7 +6,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INSPINIA | Basic Form</title>
+    <title>Create Inventory</title>
    <script src="scripts/jquery-3.1.1.min.js"></script>
 	<!-- Toastr -->
 	<link href="styles/plugins/toastr/toastr.min.css" rel="stylesheet">
@@ -55,8 +55,14 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 	.control-label{
 		font-weight:normal;
 		padding-top:0px !important;
+		font-size:12px;
+		line-height:14px;
 	}
-	
+	.form-control{
+		padding:4px;
+		font-size:12px;
+		height:30px !important;
+	}
 	</style>
 </head>
 
@@ -67,30 +73,35 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
         <div class="sidebar-collapse">
             <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
-                    <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
-                             </span>
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
-                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                            <li><a href="profile.html">Profile</a></li>
-                            <li><a href="contacts.html">Contacts</a></li>
-                            <li><a href="mailbox.html">Mailbox</a></li>
-                            <li class="divider"></li>
-                            <li><a href="login.html">Logout</a></li>
-                        </ul>
+                    <div class="dropdown profile-element"> 
+                    	<span>
+                            Sareen Properties
+                        </span>
+
                     </div>
-                    <div class="logo-element">
-                        IN+
-                    </div>
+
                 </li>
                 <li>
-                    <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Inventory</span> <span class="fa arrow"></span></a>
-                    <ul class="nav nav-second-level collapse">
-                        <li><a href="createInventory.php">Create</a></li>
-                    </ul>
-                </li>  
+                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
+                    	<span class="nav-label">Create Property</span>  
+                    </a>
+                </li>
+                 <li>
+                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
+                    	<span class="nav-label">Show Properties</span>  
+                    </a>
+                </li>
+                 <li>
+                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
+                    	<span class="nav-label">Change Password</span>  
+                    </a>
+                </li>
+                 <li>
+                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
+                    	<span class="nav-label">Logout</span>  
+                    </a>
+                </li>
+                
             </ul>
 
         </div>
@@ -98,124 +109,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 
         <div id="page-wrapper" class="gray-bg">
         <div class="row border-bottom">
-        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-            <form role="search" class="navbar-form-custom" action="search_results.html">
-                <div class="form-group">
-                    <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
-                </div>
-            </form>
-        </div>
-            <ul class="nav navbar-top-links navbar-right">
-                <li>
-                    <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                        <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-messages">
-                        <li>
-                            <div class="dropdown-messages-box">
-                                <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/a7.jpg">
-                                </a>
-                                <div class="media-body">
-                                    <small class="pull-right">46h ago</small>
-                                    <strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
-                                    <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <div class="dropdown-messages-box">
-                                <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/a4.jpg">
-                                </a>
-                                <div class="media-body ">
-                                    <small class="pull-right text-navy">5h ago</small>
-                                    <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
-                                    <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <div class="dropdown-messages-box">
-                                <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/profile.jpg">
-                                </a>
-                                <div class="media-body ">
-                                    <small class="pull-right">23h ago</small>
-                                    <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
-                                    <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <div class="text-center link-block">
-                                <a href="mailbox.html">
-                                    <i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                        <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-alerts">
-                        <li>
-                            <a href="mailbox.html">
-                                <div>
-                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
-                                    <span class="pull-right text-muted small">4 minutes ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="profile.html">
-                                <div>
-                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                    <span class="pull-right text-muted small">12 minutes ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="grid_options.html">
-                                <div>
-                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                    <span class="pull-right text-muted small">4 minutes ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <div class="text-center link-block">
-                                <a href="notifications.html">
-                                    <strong>See All Alerts</strong>
-                                    <i class="fa fa-angle-right"></i>
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-
-
-                <li>
-                    <a href="login.html">
-                        <i class="fa fa-sign-out"></i> Log out
-                    </a>
-                </li>
-            </ul>
-
-        </nav>
+        
         </div>
             
          <div class="row">
@@ -341,10 +235,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 									</label>
 							</div>
 							
-							<hr>
-							House Fields
-							<hr>
-							<div class="form-group">
+							<div class="form-group furnishing" style="display:none">
 								<label class="col-sm-1 control-label">Furnishing</label>
 								<div class="col-sm-5">
 									<?php echo DropDownUtils::getFurnishingDD("furnishing", "", "")?>
@@ -355,7 +246,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 								</div>
 							</div>
 							
-							<div class="form-group">
+							<div class="form-group stories" style="display:none">
 								<label class="col-sm-1 control-label">Stories</label>
 								<div class="col-sm-2">
 									<input class="form-control" type="text" id="stories" name="stories">
@@ -366,10 +257,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 								</div>
 							</div>
 							
-							<hr>
-							Agricultural Land
-							<hr>
-							<div class="form-group">
+							<div class="form-group agriculturalLand" style="display:none">
 								<label class="col-sm-1 control-label">Sellers</label>
 								<div class="col-sm-2">
 									<input class="form-control" type="text" id="totalsellers" name="totalsellers">
@@ -388,17 +276,17 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 							</div>
 							
 							
-							<hr>
-							Other for Building/Mall/Floor/Bank etc
-							<hr>
-							<div class="form-group">
+							<div class="form-group floorNumber" style="display:none">
 								<label class="col-sm-1 control-label">Floor Number</label>
-								<div class="col-sm-5">
+								<div class="col-sm-3">
 									<input class="form-control" type="text" id="floornumber" name="floornumber">
 								</div>
+							</div>
+							
+							<div class="form-group specifications" style="display:none">
 								<label class="col-sm-1 control-label">Specifications</label>
-								<div class="col-sm-5">
-									<textarea rows="3" cols="4" class="form-control" name="specifications"></textarea>
+								<div class="col-sm-11">
+									<textarea rows="10" cols="4" class="form-control" name="specifications" style="height:50px !important"></textarea>
 								</div>
 							</div>
 							
@@ -435,6 +323,54 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
             	    var btn = this;
             	    ValidateAndSave(e,btn);           
             	});
+                $( "#propertytype" ).change(function() {
+                		$(".furnishing").hide();
+						$(".stories").hide();
+						$(".agriculturalLand").hide();
+						$(".floorNumber").hide();
+						$(".specifications").hide();
+						
+						if(this.value == "building"){
+							$(".furnishing").show();
+							$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+                   		}else if(this.value == "house"){
+                   			$(".furnishing").show();
+							$(".stories").show();
+							$(".specifications").show();
+                       	}else if(this.value == "plot"){
+                       		$(".agriculturalLand").show();
+							$(".specifications").show();
+                       	}else if(this.value == "bank"){
+                       		$(".furnishing").show();
+							$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+                        }else if(this.value == "floor"){
+                        	$(".floorNumber").show();
+							$(".specifications").show();
+                        }else if(this.value == "sco"){
+                        	$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+                        }else if(this.value == "mallShop"){
+                        	$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+                        }else if(this.value == "godown"){
+                        	$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+                        }else if(this.value == "parkingArea"){
+                        	$(".specifications").show();
+                        }else if(this.value == "foodLounge"){
+                        	$(".stories").show();
+							$(".floorNumber").show();
+							$(".specifications").show();
+
+                        }
+                })
             });
             $('#inventoryForm').jqxValidator({
             	hintType: 'label',
@@ -442,7 +378,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
             	rules: [
             	{ input: '#address1', message: 'Address1 is required!', action: 'keyup, blur', rule: 'required' },
             	{ input: '#contactPerson', message: 'Name is required!', action: 'keyup, blur', rule: 'required' },
-            	{ input: '#contactMobile', message: 'Mobile11 is required!', action: 'keyup, blur', rule: 'required' }
+            	{ input: '#contactMobile', message: 'Mobile is required!', action: 'keyup, blur', rule: 'required' }
             	]
             });
             function submit(e,btn){
