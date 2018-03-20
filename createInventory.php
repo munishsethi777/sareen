c9b8a1a98a6e91300e74be624fc0adfe75f750e9
munishsethi777 <munishsethi777@gmail.com>
2018-03-20 10:54:56 +0530

    menu changes

--- createInventory.php
+++ createInventory.php
@@ -69,43 +69,7 @@ require_once ($ConstantsArray ['dbServerUrl'] . "Utils/DropdownUtil.php");
 <body>
 
     <div id="wrapper">
-    <nav class="navbar-default navbar-static-side" role="navigation">
-        <div class="sidebar-collapse">
-            <ul class="nav metismenu" id="side-menu">
-                <li class="nav-header">
-                    <div class="dropdown profile-element"> 
-                    	<span>
-                            Sareen Properties
-                        </span>
-
-                    </div>
-
-                </li>
-                <li>
-                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
-                    	<span class="nav-label">Create Property</span>  
-                    </a>
-                </li>
-                 <li>
-                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
-                    	<span class="nav-label">Show Properties</span>  
-                    </a>
-                </li>
-                 <li>
-                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
-                    	<span class="nav-label">Change Password</span>  
-                    </a>
-                </li>
-                 <li>
-                    <a href="createInventory.php"><i class="fa fa-th-large"></i> 
-                    	<span class="nav-label">Logout</span>  
-                    </a>
-                </li>
-                
-            </ul>
-
-        </div>
-    </nav>
+        <?php include("menuInclude.php")?>     
 
         <div id="page-wrapper" class="gray-bg">
         <div class="row border-bottom">
