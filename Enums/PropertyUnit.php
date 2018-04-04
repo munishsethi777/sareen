72accc0b844c3fe5691199936879e60ac0f1eecd
munishsethi777 <munishsethi777@gmail.com>
2018-04-04 13:24:35 +0530

    propertyUnit new enum

---
+++ Enums/PropertyUnit.php
@@ -0,0 +1,12 @@
+<?php
+require_once($ConstantsArray['dbServerUrl'] ."Enums/BasicEnum.php");
+class PropertyUnit extends BasicEnum{
+	const squareFeet = "Sq-ft";
+	const squareYard = "Sq-yrd";
+	const squareMeter = "Sq-m";
+	const acre = "Acre";
+	const bigha = "Bigha";
+	const hectare = "Hectare";
+	const marla = "Marla";
+	const kanal = "Kanal";
+}
